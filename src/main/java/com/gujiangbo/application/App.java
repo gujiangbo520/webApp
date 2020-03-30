fc8917e9bd7a01edc0a63864cae651a73b313cd1
gujiangbo <646832012@qq.com>
2020-03-30 09:07:16 +0800

    启动类添加

--- src/main/java/com/gujiangbo/application/App.java
+++ src/main/java/com/gujiangbo/application/App.java
@@ -1,8 +1,12 @@
 package com.gujiangbo.application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("hello world !");
+        SpringApplication.run(App.class,args);
     }
 }
